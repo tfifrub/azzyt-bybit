7fbe24ea2744bfcd8eb3357e7744a4d0b4f5a6cd
tfifrub <tfifrub690000@outlook.com>
2024-06-30 20:13:58 +0200

    Update __init__.py
    Maybe you find too that is more convenient with examples?

--- videos/__init__.py
+++ videos/__init__.py
@@ -14,3 +14,11 @@ def ts_dt(time : int, is_ms = True):
     if is_ms: time = int(time / 1000)
 
     return datetime.fromtimestamp(time, tz=pytz.UTC)
+
+timestamp_ms = 1719777600000
+dt_object_ms = ts_dt(timestamp_ms)
+print("Milliseconds timestamp: ", dt_object_ms)
+
+timestamp_s = 1719777600
+dt_object_s = ts_dt(timestamp_s, is_ms=False)
+print("Seconds timestamp: ", dt_object_s)
